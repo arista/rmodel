b599fb2d31bae460a73cf6533debb1c342cc11c2
arista <arista@abramsons.info>
2019-04-15 09:41:09 -0400

    add comment

--- src/RMComputed.ts
+++ src/RMComputed.ts
@@ -2,6 +2,15 @@
 // created by calling RModel.computed.  If an instance of this is
 // assigned to an object property, that will be equivalent to calling
 // addComputedProperty with the values specified here.
+//
+// This facility is provided as a convenience, allowing a kind of
+// "shorthand" for defining RModel values:
+//
+// const r = RModel({
+//   x: 10,
+//   y: 20,
+//   sum: RModel.computed(v=>v.x + v.y)
+// })
 import {ComputedPropertyOptions} from './Types'
 
 export default class RMComputed<T,R> {
